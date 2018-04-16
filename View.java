6429ca19f1103e87592ce02fe32f280943614b19
Wang Peiyu <oliviapy@udel.edu>
2018-04-16 13:47:39 -0400

    first commitment

---
+++ View.java
@@ -0,0 +1,21 @@
+
+public class View {
+
+	final static int frameWidth = 700;
+    final static int frameHeight = 700;
+    final static int imgWidth = 165;
+    final static int imgHeight = 165;
+    
+	public int getWidth(){
+		return frameWidth;
+	}
+	public int getHeight(){
+		return frameHeight;
+	}
+	public int getImageWidth(){
+		return imgWidth;
+	}
+	public int getImageHeight(){
+		return imgHeight;
+	}
+}
